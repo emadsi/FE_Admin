f7aa72ea0005937c352e0ce37d9516cbb4f5047d
emadsi <emad.silawi89@gmail.com>
2019-09-05 23:01:25 +0300

    Completing Components

--- UsersTable.js
+++ UsersTable.js
@@ -8,98 +8,91 @@ export default class UsersTable extends React.Component{
 
     render(){
         return(
-            <div className="container">
-                <div className="row justify-content-md-center">
-                    <div className="col-md-1 ml-12">
-                        <h1>Users</h1>
+            <div>
+                <div className="row justify-content-center">
+                    <h1 className="alert alert-light" role="alert">
+                        Users
+                    </h1>
+                </div>
+                
+                <div className="row offset-md-4 mb-2 col-md-3 mg-auto">
+                    <div className="card">
+                        <div className="card-body">
+                            This is some text within a card body.
+                        </div>
                     </div>
                 </div>
-
-                <div className="row">
-                    <form onSubmit="">
-                        <div className="col-4">
-                            <input className="form-control mr-sm-2" id="search-bar" type="search" placeholder="Search a user" aria-label="Search" />
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-
-                            <div className="search-users" id="advanced-search">
-                                <a id="advanced-btn" onclick={this.showOptions}>Advanced Search</a>
+                
+                <div className="row justify-content-md-center ">
+                    <form>
+                        <div className="input-group lg-10">
+                            <input type="text" className="form-control" placeholder="Search by name" aria-label="Search by name" aria-describedby="button-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-success" type="button" id="button-addon2">Button</button>
                             </div>
-
-                            <div className="row"> 
-                                <div className="dropdown mr-1 col">
-                                    <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
-                                        Role
-                                    </button>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                        <a className="dropdown-item" href="#">Manager</a>
-                                        <a className="dropdown-item" href="#">Employee</a>
-                                        <a className="dropdown-item" href="#">Admin</a>
-                                    </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="dropdown mr-1 mt-2">
+                                <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+                                    Role
+                                </button>
+                                <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                                    <a className="dropdown-item" href="#">Manager</a>
+                                    <a className="dropdown-item" href="#">Employee</a>
+                                    <a className="dropdown-item" href="#">Admin</a>
                                 </div>
-
-                                <div className="dropdown mr-1 col">
+                            </div>
+                            <div className="dropdown mr-1 mt-2">
                                     <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
                                         Department
                                     </button>
                                     <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                        <a className="dropdown-item" href="#">Manager</a>
-                                        <a className="dropdown-item" href="#">Employee</a>
-                                        <a className="dropdown-item" href="#">Admin</a>
+                                      <a className="dropdown-item" href="#">Manager</a>
+                                      <a className="dropdown-item" href="#">Employee</a>
+                                      <a className="dropdown-item" href="#">Admin</a>
                                     </div>
-                                </div>
-
-                                <div className="dropdown mr-1 col">
+                            </div>
+                            <div className="dropdown mr-1 mt-2">
                                     <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
                                         Work Site
                                     </button>
                                     <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                        <a className="dropdown-item" href="#">Manager</a>
-                                        <a className="dropdown-item" href="#">Employee</a>
-                                        <a className="dropdown-item" href="#">Admin</a>
+                                      <a className="dropdown-item" href="#">Manager</a>
+                                      <a className="dropdown-item" href="#">Employee</a>
+                                      <a className="dropdown-item" href="#">Admin</a>
                                     </div>
-                                </div>
-
-                                <div className="dropdown mr-1 col">
+                            </div>
+                            <div className="dropdown mr-1 mt-2">
                                     <button type="button" className="btn btn-secondary dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
                                         Country
                                     </button>
                                     <div className="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                        <a className="dropdown-item" href="#">Manager</a>
-                                        <a className="dropdown-item" href="#">Employee</a>
-                                        <a className="dropdown-item" href="#">Admin</a>
+                                      <a className="dropdown-item" href="#">Manager</a>
+                                      <a className="dropdown-item" href="#">Employee</a>
+                                      <a className="dropdown-item" href="#">Admin</a>
                                     </div>
-                                </div>
                             </div>
-                        </div>  
+                        </div>
                     </form>
                 </div>
 
-                {/* <!-- Adding new user --> */}
-                <div class="row">
-                    <div class="col"></div>
-                    <div class="col"></div><div class="col-2">
-                        <button class="btn btn-outline-info" id="add-user-btn">Add user</button>
-                    </div>
-                </div>
-
-                {/* <!-- Users' table --> */}
-                <div className="table table-hover table-style row justify-content-md-center mt-4">
-                    <table id="table-content">
-                        <thead>
-                            <th>ID</th>
-                            <th>Full Name</th>
-                            <th>Roles</th>
-                            <th>Department</th>
-                            <th>Work Site</th>
+                <div className="row - justify-content-center">
+                    <table className="table  col-md-6 mt-4" id="usersTable">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th>ID</th>
+                                <th>Full Name</th>
+                                <th>Roles</th>
+                                <th>Department</th>
+                                <th>Work Site</th>
+                            </tr>
                         </thead>
-                        <tbody id="tbody">
-            
+                        <tbody>
+                            <UsersTableComponent />
                         </tbody>
-                    </table>   
+                    </table>
                 </div>
-
-
-
+            
             </div>
         );
     }
